4cf79c3fabd070da26fc1c802e938c807a6fe3b0
sterlingreverse9 <sterlingreverse9@gmail.com>
2026-09-26 06:03:50 +0530

    Create handlers/interactions.js

---
+++ handlers/interactions.js
@@ -0,0 +1,6 @@
+async function handleInteractions(interaction, client) {
+    if (!interaction.isButton()) return;
+    // UI Interaction handlers can be expanded here
+}
+
+module.exports = { handleInteractions };
